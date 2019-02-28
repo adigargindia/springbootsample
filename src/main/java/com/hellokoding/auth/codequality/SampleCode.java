0503ec2904450e142fda027d23c324c225163ea4
cpdofexampublic <cpdofexam@gmail.com>
2019-02-28 18:04:10 +0530

    codequality 4 commit

--- src/main/java/com/hellokoding/auth/codequality/SampleCode.java
+++ src/main/java/com/hellokoding/auth/codequality/SampleCode.java
@@ -2,10 +2,13 @@ package com.hellokoding.auth.codequality;
 
 public class SampleCode {
 
-	String[] arrayOfStrings = {"A","B","C"};
-	String str = "";
-	for (int i = 0; i < arrayOfStrings.length ; ++i) {
-  		str = str + arrayOfStrings[i];
-	}
 
+	public void codeError(){
+		String[] arrayOfStrings = {"A","B","C"};
+		String str = "";
+		for (int i = 0; i < arrayOfStrings.length ; ++i) {
+  			str = str + arrayOfStrings[i];
+		}
+
+	}
 }
