30a7baa24a2cd12eb38519ef6d0b296bfa5c2fb8
cpdofexampublic <cpdofexam@gmail.com>
2019-02-28 17:50:21 +0530

    second commit

---
+++ src/main/java/com/hellokoding/auth/codequality/SampleCode.java
@@ -0,0 +1,10 @@
+package com.hellokoding.auth.codequality;
+
+public class SampleCode {
+
+String str = "";
+for (int i = 0; i < arrayOfStrings.length ; ++i) {
+  str = str + arrayOfStrings[i];
+}
+
+}
